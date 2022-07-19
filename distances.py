0b1cd695c0fb9b3199629aab6e88bef97470fce1
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2022-07-19 16:02:11 +0530

    add code to introduce error in chi0

--- distances.py
+++ distances.py
@@ -20,12 +20,6 @@ def chi2_distance(A, B):
     return chi 
 
 
-distance_function = mse_distance_function
-# distance_function = chi2_distance
-# distance_function = wasserstein_distance
-
-distance_function_name = str(distance_function.__name__)
-
 Cu_location = [0, 0, 0]
 O_location = [2.1, 0, 0]
 
@@ -68,6 +62,17 @@ Cu_location = [0, 0, 0]
 O_location = [2.0, 0, 0]
 k0, chi0 = generate_plot(Cu_location=Cu_location, O_location=O_location, input_home="feff_inputs", output_home="outputs", apply_fourier_transform=False)
 
+
+introduce_error = True
+error_string = ""
+if introduce_error:
+    mu, sigma = 0, 0.001 # mean and standard deviation
+    error_string = f"_error_{str(mu)}_{str(sigma)}"
+
+    error_array = np.random.normal(mu, sigma, len(chi0))
+    chi0 = chi0 + error_array
+    print(chi0-error_array)
+
 test_positions = {0: [], 1:[], 2:[]}
 
 for i in range(3):
@@ -83,28 +88,33 @@ index_to_coordinate = {
     2: "z"
 }
 
-pprint(test_positions)
+# pprint(test_positions)
 
-for i in range(3):
-    x_axis = []
-    y_axis = []
-    for position in test_positions[i]:
-        x = position[i]
-        x_axis.append(x)
-        k, chi = get_k_and_chi(Cu_location=Cu_location, O_location=position, input_home="feff_inputs", output_home="outputs", apply_fourier_transform=False)
-        distance = distance_function(chi, chi0)
-        y = distance
-        y_axis.append(y)
+distance_functions = [mse_distance_function, chi2_distance, wasserstein_distance]
+# distance_function = distance_functions[0]
+for distance_function in distance_functions:
+    distance_function_name = str(distance_function.__name__)
 
-    fig, ax = plt.subplots()
+    for i in range(3):
+        x_axis = []
+        y_axis = []
+        for position in test_positions[i]:
+            x = position[i]
+            x_axis.append(x)
+            k, chi = get_k_and_chi(Cu_location=Cu_location, O_location=position, input_home="feff_inputs", output_home="outputs", apply_fourier_transform=False)
+            distance = distance_function(chi, chi0)
+            y = distance
+            y_axis.append(y)
 
-    plt.subplots_adjust(left=0.15)
-    ax.plot(x_axis, y_axis, **{'color': 'black', 'marker': '.'})
+        fig, ax = plt.subplots()
 
-    ax.set_title(f"{distance_function_name} from Cu: {str(Cu_location)} and O: {str(O_location)}")
-    ax.set_xlabel(f"position in the co-ordinate {index_to_coordinate[i]}")
-    ax.set_ylabel(f"distance from {str(O_location)}")
+        plt.subplots_adjust(left=0.15)
+        ax.plot(x_axis, y_axis, **{'color': 'black', 'marker': '.'})
 
-    figure_path = f"{index_to_coordinate[i]}_co-ordinate_{distance_function_name}.png"
-    plt.savefig(figure_path)
-    plt.close(fig)
+        ax.set_title(f"{distance_function_name} from Cu: {str(Cu_location)} and O: {str(O_location)}")
+        ax.set_xlabel(f"position in the co-ordinate {index_to_coordinate[i]}")
+        ax.set_ylabel(f"distance from {str(O_location)}")
+
+        figure_path = f"{index_to_coordinate[i]}_co-ordinate_{distance_function_name}{error_string}.png"
+        plt.savefig(figure_path)
+        plt.close(fig)
